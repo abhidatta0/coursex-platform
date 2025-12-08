b5f147d6bd53d576d553270c2a7c1a623ee65d92
abhidatta0 <abhidatta146@gmail.com>
2025-12-08 13:11:48 +0530

    installed shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 const App = ()=> {
 
   return (
     <div className="text-3xl font-bold underline">
       Hello App
+      <Button>Click me</Button>
     </div>
   )
 }
